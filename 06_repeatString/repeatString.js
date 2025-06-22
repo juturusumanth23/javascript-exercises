31f172f593ad367570efb28e1278fdcd90db7121
Xumanth <juturusumanth23@gmail.com>
2025-06-22 11:31:39 +0530

    Repeats the string given number of times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let result= num>=0 ? '' : 'ERROR';
+    for (i=0;i<num;i++) {
+        result+=string;
+    }
+    return result
 };
 
 // Do not edit below this line
