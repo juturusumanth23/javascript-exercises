2bec01f183dfcec94f94be0833a484d72b3092c1
Xumanth <juturusumanth23@gmail.com>
2025-06-22 18:59:26 +0530

    Return the sum of integers between two numbers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if (a<0 || b<0 || Math.floor(a)!=a || Math.floor(b)!=b || parseInt(a)!==a || parseInt(b)!==b) {
+        return 'ERROR';
+    }
+    return a<b ? (b*(b+1)-a*(a-1))/2 : (a*(a+1)-b*(b-1))/2 ;
 };
 
 // Do not edit below this line
