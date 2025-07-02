7c97e5a1f65d1e3f742228bf3827ecd554457d9b
Xumanth <juturusumanth23@gmail.com>
2025-07-02 23:59:47 +0530

    Result the oldest person from given array

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    return arr.reduce((oldest,item)=> {
+        oldest.yearOfDeath = oldest.yearOfDeath ? oldest.yearOfDeath : new Date().getFullYear();
+        item.yearOfDeath = item.yearOfDeath ? item.yearOfDeath : new Date().getFullYear();
+
+        return (oldest.yearOfDeath-oldest.yearOfBirth>item.yearOfDeath-item.yearOfBirth ? oldest : item)
+});
 };
 
 // Do not edit below this line
