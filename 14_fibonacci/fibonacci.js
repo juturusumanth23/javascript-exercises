77df2b19098c934f7c2abf89092b65cbc60bba71
Xumanth <juturusumanth23@gmail.com>
2025-07-02 22:44:17 +0530

    Return a specific member of the Fibonacci sequence

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number<0) {
+        return "OOPS";
+    }
+    let curr = 0;
+    let next = 1;
+    for (let i=0;i<number;i++) {
+        let temp = curr;
+        curr = next;
+        next = temp + next;
+    }
+    return curr;
 };
 
 // Do not edit below this line
