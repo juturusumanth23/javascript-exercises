a68c551058c1f017fcd2e06586dea06be53efc4a
Xumanth <juturusumanth23@gmail.com>
2025-07-02 22:19:25 +0530

    Return whether input string is palindrome or not

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let alphanumerical = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    let cleanedString=str
+        .toLowerCase()
+        .split("")
+        .filter(item=>alphanumerical.includes(item))
+        .join("")
+    let reversedString = cleanedString
+        .split("")
+        .reverse()
+        .join("");
 
+    return cleanedString==reversedString;
 };
 
 // Do not edit below this line
